74f100b6a1a47568cd34f592698a1f2e45e94433
Your Name <you@example.com>
2016-10-17 22:39:09 -0700

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisApplication
+{
+    class ThisGame
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the Magiiiic Number! oooOOOooh!");
+            int magicNumber = new Random().Next(1, 51); // Random number from 1 - 50
+            int initialGuess = 0; // incorrect guess
+            while (initialGuess != magicNumber) // execute loop until true
+            {
+                initialGuess = int.Parse(Console.ReadLine()); // integer values
+                if(initialGuess < magicNumber) // if lower than magicNumber
+                {
+                    Console.WriteLine("No, Higher!");
+                }
+                if(initialGuess > magicNumber) // if higher than magicNumber
+                {
+                    Console.WriteLine("No, Lower!");
+                }
+            }
+            Console.WriteLine("Si, estas correct!");
+        }
+    }
+}
